6faab546bab8c8ac6b502e0c84b7d6120e7e4348
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2025-12-27 00:07:29 +0530

    folders

---
+++ src/pages/Dashborad/DashboardRouter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardRouter = () => {
+  return (
+    <div>DashboardRouter</div>
+  )
+}
+
+export default DashboardRouter
---
+++ src/pages/Dashborad/blind/BlindDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlindDashboard = () => {
+  return (
+    <div>BlindDashboard</div>
+  )
+}
+
+export default BlindDashboard
