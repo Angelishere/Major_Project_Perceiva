486b053bd1ebb90161b05bc0cab88b1a45978703
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2025-12-29 16:05:41 +0530

    token fix

--- Server/index.js
+++ Server/index.js
@@ -128,7 +128,13 @@ app.post("/register", async (req, res) => {
 
     }
 
-    res.status(201).json({ message: "User registered successfully" });
+    const token = jwt.sign(
+      { userId: user._id, username: user.username, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES || "1h" }
+    );
+
+    res.status(201).json({ message: "User registered successfully", token:token });
     console.log(`Account created for username: "${username}"`);
 
   } catch (error) {
