c08a99a11e421dd9be59de0bf0b1e8a53b6e4c66
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2026-03-10 09:55:12 +0530

    report, research paper and android

---
+++ Android/perceiva-app/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello guys this is my first appa</Text>
+    </View>
+  );
+}
